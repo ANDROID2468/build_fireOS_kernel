7e39daff94851c9876db7898d0fbcfc3e18c9eb2
ANDROID2468 <nicholas.steyer@gmail.com>
2020-04-18 17:46:46 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Building fire os kernel 
+This is using  [ggrow's kernel source](https://github.com/ggow/android_kernel_amazon_mt8127-common/tree/amazon-5364)
+
+# downlowing instructions
+
+```
+git clone https://github.com/ANDROID2468/build_fireOS_kernel.git -b mt8127
+cd build_fireOS_kernel
+```
+
+# Building instructions 
+
+Austin
+
+run this commmand:
+```
+./austin/build_kernel.sh kernel.tar out
+```
+
+Ford
+run this commmand:
+```
+./ford/build_kernel.sh kernel.tar out
+```
---
+++ austin/build_kernel_config.sh
@@ -0,0 +1,13 @@
+################################################################################
+#
+#  build_kernel_config.sh
+#
+#  Copyright (c) 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+################################################################################
+
+KERNEL_SUBPATH="kernel/mediatek/mt8127/3.10"
+DEFCONFIG_NAME="austin_defconfig"
+TARGET_ARCH="arm"
+TOOLCHAIN_REPO="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8"
+TOOLCHAIN_PREFIX="arm-eabi-"
